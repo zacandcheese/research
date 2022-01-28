61c51a7e7aa6c256a81e6e1fd3a27de1153a018c
zacandcheese <zacandcheese@icloud.com>
2022-01-28 15:08:21 -0500

    [DATALAD] Recorded changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis of the emotions and discourse in the show Friends',
+    author='Zachary Nowak',
+    license='MIT',
+)
